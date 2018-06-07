5d8f51b3069bd26696f27f3c6f9c7b1e84353e97
Dine <ariadne0288@hotmail.com>
2018-06-06 23:30:24 -0300

    Cifra funciona com qualquer key

--- Cifra de Cesar/app.js
+++ Cifra de Cesar/app.js
@@ -14,7 +14,7 @@ do {
 } while (regularMsg === "" || regularMsg === null);
 
 var encryptedMsg = "";
-var key = 33;
+var key = 3469;
 function cipher() {
   for(i = 0; i < regularMsg.length; i++) {
     var character = regularMsg.charCodeAt(i);
@@ -40,16 +40,17 @@ function decipher() {
     var cipheredCharacter = encryptedMsg.charCodeAt(i);
 // Verifica se o caractere é maiusculo e aplica fórmula correspondente
     if(cipheredCharacter < 91) {
-      var decipheredCharacter = ((cipheredCharacter - key) % 26 + (65 % 26)) % 26 + 65;
+      var decipheredCharacter = ((cipheredCharacter - 64 - key)*(-1)) % 26 + 65;
       decipheredCharacter = String.fromCharCode(decipheredCharacter);
       decipheredMsg += decipheredCharacter;
 // Aplica fórmula para caractere minusculo
     } else {
-      var decipheredCharacter = ((cipheredCharacter - key) % 26 + (97 % 26) + 14) % 26 + 97;
+      var decipheredCharacter = ((cipheredCharacter - 96 - key)*(-1)) % 26 + 97;
       decipheredCharacter = String.fromCharCode(decipheredCharacter);
       decipheredMsg += decipheredCharacter;
     }
   }
+  decipheredMsg = decipheredMsg.split('').reverse().join('');
 //Imprime resultado na tela
   document.getElementById("reverse").innerHTML = decipheredMsg;
 }
